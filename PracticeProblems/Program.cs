a787677f221ed27784c65cf4e8de0491c32649db
Jennyf19 <ferriesj@gmail.com>
2016-07-29 12:12:25 -0700

    add student score section

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PracticeProblems
 {
@@ -10,6 +7,48 @@ namespace PracticeProblems
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your student's score: ");
+            int studentScore = int.Parse(Console.ReadLine());
+
+            if (studentScore >= 90)
+                Console.WriteLine("The student's letter grade for a(n) {0} is an A", studentScore);
+            if (studentScore >= 70 && studentScore < 90)
+                Console.WriteLine("The student's letter grade for a(n) {0} is a B", studentScore);
+            if (studentScore >= 50 && studentScore < 70)
+                Console.WriteLine("The student's letter grade for a(n) {0} is a C", studentScore);
+            if (studentScore < 50)
+                Console.WriteLine("The student's letter grade for a(n) {0} is an F", studentScore);
+            else
+            {
+                Console.WriteLine("Please renter the grade.");
+            }
+            Console.ReadLine();
+
+            /*int x, y, z;
+            Console.WriteLine("Please enter three values");
+            Console.WriteLine("Value for integer 1: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Value for integer 2: ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Value for integer 3: ");
+            z = int.Parse(Console.ReadLine());
+            Console.WriteLine("You entered the following integers {0}, {1}, {2}", x, y, z);
+            if (x > y)
+                Console.WriteLine(x + " is greater than " + y);
+            if (x > z)
+                Console.WriteLine(x + " is greater than " + z);
+            if (y > x)
+                Console.WriteLine(y + " is greater than " + x);
+            if (z > x)
+                Console.WriteLine(z + " is greater than " + x);
+            if (y > z)
+                Console.WriteLine(y + " is greater than " + z);
+            if (z > y)
+                Console.WriteLine(z + " is greater than " + y);
+            else 
+                Console.WriteLine("I'm not sure how to compare those numbers");
+            Console.ReadLine();
+            */
         }
     }
 }
